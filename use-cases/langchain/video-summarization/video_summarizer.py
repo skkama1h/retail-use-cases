dc8e8f339df04f81c00ce9067bff60fdfcc8e713
Sanjana Kamath <sanjana.k.kamath@intel.com>
2025-02-21 11:18:42 -0800

    Reformat files as per PEP guidelines for ease of reading

--- use-cases/langchain/video-summarization/video_summarizer.py
+++ use-cases/langchain/video-summarization/video_summarizer.py
@@ -1,17 +1,18 @@
+import argparse
 import os
 import sys
 import time
-import argparse
-import openvino_genai
+
 from langchain.prompts import PromptTemplate
-from ov_lvm_wrapper import OVMiniCPMV26Worker
 from langchain_community.document_loaders.video import VideoChunkLoader
+
+from ov_lvm_wrapper import OVMiniCPMV26Worker
+
 
 def output_handler(text: str,
                    filename: str = '',
                    mode: str = 'w',
                    verbose: bool = True):
-    
     # Print to terminal
     if verbose:
         print(text)
@@ -21,6 +22,7 @@ def output_handler(text: str,
         with open(filename, mode) as FH:
             print(text, file=FH)
 
+
 if __name__ == '__main__':
     # Parse inputs
     parser_txt = "Generate video summarization using Langchanin, OpenVINO-genai, and MiniCPM-V-2_6."
@@ -28,13 +30,13 @@ if __name__ == '__main__':
     parser.add_argument("video_file", type=str,
                         help='Path to video you want to summarize.')
     parser.add_argument("model_dir", type=str,
-                        help="Path to openvino-genai optimized model")    
+                        help="Path to openvino-genai optimized model")
     parser.add_argument("-p", "--prompt", type=str,
                         help="Text prompt. By default set to: `Please summarize this video.`",
                         default="Please summarize this video.")
     parser.add_argument("-d", "--device", type=str,
                         help="Target device for running ov MiniCPM-v-2_6",
-                        default="CPU")    
+                        default="CPU")
     parser.add_argument("-t", "--max_new_tokens", type=int,
                         help="Maximum number of tokens to be generated.",
                         default=500)
@@ -57,23 +59,23 @@ if __name__ == '__main__':
     if not os.path.exists(args.video_file):
         print(f"{args.video_file} does not exist.")
         exit()
-    
+
     # Create template for inputs
     prompt = PromptTemplate(
         input_variables=["video", "question"],
         template="{video},{question}"
     )
-        
+
     # Wrap OpenVINO-GenAI optimized model in custom langchain wrapper
     resolution = [] if not args.resolution else args.resolution
-    ov_minicpm = OVMiniCPMV26Worker(model_dir = args.model_dir,
-                                    device = args.device,
-                                    max_new_tokens = args.max_new_tokens,
-                                    max_num_frames = args.max_num_frames,
-                                    resolution = resolution)
-    
+    ov_minicpm = OVMiniCPMV26Worker(model_dir=args.model_dir,
+                                    device=args.device,
+                                    max_new_tokens=args.max_new_tokens,
+                                    max_num_frames=args.max_num_frames,
+                                    resolution=resolution)
+
     # Create pipeline and invoke
-    chain =  prompt | ov_minicpm
+    chain = prompt | ov_minicpm
 
     # Initialize video chunk loader
     loader = VideoChunkLoader(
@@ -86,7 +88,7 @@ if __name__ == '__main__':
     output_handler("python " + " ".join(sys.argv),
                    filename=args.outfile, mode='w',
                    verbose=False)
-    
+
     # Loop through docs and generate chunk summaries    
     chunk_summaries = []
     for doc in loader.lazy_load():
@@ -95,22 +97,26 @@ if __name__ == '__main__':
                        filename=args.outfile, mode='a')
         output_handler(str(f"Chunk Content: {doc.page_content}"),
                        filename=args.outfile, mode='a')
-        
+
         # Generate sumarries
         chunk_st_time = time.time()
-        inputs = {"video": doc.metadata['chunk_path'], "question": args.prompt}        
+        inputs = {"video": doc.metadata['chunk_path'], "question": args.prompt}
         output = chain.invoke(inputs)
 
         # Log output
         output_handler(output, filename=args.outfile, mode='a', verbose=False)
-        chunk_summaries.append(f"Start time: {doc.metadata['start_time']} End time: {doc.metadata['end_time']}\n" + output)
-        output_handler("\nChunk Inference time: {} sec\n".format(time.time() - chunk_st_time), filename=args.outfile, mode='a')
-    
+        chunk_summaries.append(
+            f"Start time: {doc.metadata['start_time']} End time: {doc.metadata['end_time']}\n" + output)
+        output_handler("\nChunk Inference time: {} sec\n".format(time.time() - chunk_st_time), filename=args.outfile,
+                       mode='a')
+
     # Summarize the full video, using the subsections summaries from each chunk
     overall_summ_st_time = time.time()
     full_summ_prompt = 'The following are summaries of subsections of a video. Each subsection summary is separated by the delimiter ">|<". Each subsection summary will start with the start and end timestamps of the subsection relative to the full video. Please create a summary of the overall video, highlighting all important information, including timestamps:\n\n{}'
     inputs = {"video": '', "question": full_summ_prompt.format("\n>|<\n".join(chunk_summaries))}
-    output = chain.invoke(inputs)    
-    output_handler("\nOverall video summary inference time: {} sec\n".format(time.time() - overall_summ_st_time), filename=args.outfile, mode='a')
-    output_handler("\nTotal Inference time: {} sec\n".format(time.time() - tot_st_time), filename=args.outfile, mode='a')
+    output = chain.invoke(inputs)
+    output_handler("\nOverall video summary inference time: {} sec\n".format(time.time() - overall_summ_st_time),
+                   filename=args.outfile, mode='a')
+    output_handler("\nTotal Inference time: {} sec\n".format(time.time() - tot_st_time), filename=args.outfile,
+                   mode='a')
     output_handler(output, filename=args.outfile, mode='a', verbose=False)
